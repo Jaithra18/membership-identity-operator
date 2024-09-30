553b277a987a2fc399b9ade73bcbac48a7894c2c
Jaithra18 <Jaithrathota@gmail.com>
2024-09-30 18:11:34 +0100

    change

--- main10.py
+++ main10.py
@@ -8,3 +8,14 @@ print(a is not b)
 x="Hello world"
 y="Hello"
 print(y in x)
+
+a1=10
+b1=5
+print("bitwise and operator",a1&b1)
+print("bitwise or operator",a1|b1)
+d=9
+print("bitwise xor operator",a1^d)
+print("bitwise not operator",~a1)
+x1=3
+print("bitwise << operator",x1<<1)
+print("bitwise >> operator",x1>>1)
